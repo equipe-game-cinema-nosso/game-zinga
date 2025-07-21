5d52b738a7652eb6ac2046dcf80050fd58621fb0
zudotaki <julianaleot@gmail.com>
2025-07-21 16:23:53 -0300

    Att programação

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
 
-    private float speed;
+    [SerializeField] private float speed = 6f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,12 +14,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
+         Move();
     }
     void Move()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(moveHorizontal * speed * Time.deltaTime, 0, 0);
+        this.gameObject.transform.position += new Vector3(moveHorizontal * speed * Time.deltaTime, 0, 0);
       
     }
 }
