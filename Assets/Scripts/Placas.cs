27c6bfe535b88669e36fa5f5b636516148bbe367
zudotaki <julianaleot@gmail.com>
2025-07-27 18:55:09 -0300

    Feat: Iluminação
    Iluminação das placas dos predios

---
+++ Assets/Scripts/Placas.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class Placas : MonoBehaviour
+{
+    public Light2D luz;
+    public float intensidadeMin = 1.5f;
+    public float intensidadeMax = 5f;
+    public float velocidade = 10f;
+    private bool aumentando = true;
+
+
+        void Update()
+
+    {
+         if (aumentando)
+            {
+            luz.intensity += velocidade * Time.deltaTime;
+            if (luz.intensity >= intensidadeMax)
+                aumentando = false;
+            }
+            else
+            {
+                luz.intensity -= velocidade * Time.deltaTime;
+                if (luz.intensity <= intensidadeMin)
+                    aumentando = true;
+            }
+    }
+}
